571c21d6895a6bef103f511eaf8e1de053382609
RusJJ <rusjj1337@gmail.com>
2021-07-12 12:55:14 +0300

    1.0.2

---
+++ GorillaFriends/FriendButton.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GorillaFriends
+{
+    /* Friend Button's Script */
+    public class FriendButton : MonoBehaviour
+    {
+        public GorillaPlayerScoreboardLine parentLine = null;
+        public bool isOn = false;
+        public string offText = "";
+        public string onText = "";
+        public Text myText = null;
+        public float touchTime = 0.0f;
+        public Material offMaterial;
+        public Material onMaterial;
+        private MeshRenderer meshRenderer = null;
+        private bool initialized = false;
+        private float nextUpdate = 0.0f;
+
+        private void Start()
+        {
+            meshRenderer = gameObject.GetComponent<MeshRenderer>();
+            if (meshRenderer == null)
+            {
+                Main.Log("MeshRenderer is missing?");
+            }
+        }
+        private void Update()
+        {
+            if (nextUpdate > Time.time || parentLine.playerVRRig == null || parentLine.linePlayer == null) return;
+            nextUpdate = Time.time + 0.5f;
+
+            /* First Initialization? */
+            if (!initialized)
+            {
+                initialized = true;
+                if (Main.IsVerified(parentLine.linePlayer.UserId))
+                {
+                    parentLine.playerName.color = Main.m_clrVerifiedColor;
+                    parentLine.playerVRRig.playerText.color = Main.m_clrVerifiedColor;
+                }
+
+                if (parentLine.linePlayer.IsLocal) gameObject.SetActive(false);
+                else
+                {
+                    if (Main.IsFriend(parentLine.linePlayer.UserId))
+                    {
+                        if (!Main.IsInFriendList(parentLine.linePlayer.UserId)) Main.m_listCurrentSessionFriends.Add(parentLine.linePlayer.UserId);
+                        parentLine.playerName.color = Main.m_clrFriendColor;
+                        parentLine.playerVRRig.playerText.color = Main.m_clrFriendColor;
+                        isOn = true;
+                        myText.text = onText;
+                        UpdateColor();
+                    }
+                }
+                return;
+            }
+
+            if (!parentLine.linePlayer.IsLocal && isOn != Main.IsInFriendList(parentLine.linePlayer.UserId))
+            {
+                isOn = !isOn;
+                UpdateColor();
+                if (!isOn)
+                {
+                    if (Main.IsVerified(parentLine.linePlayer.UserId))
+                    {
+                        parentLine.playerName.color = Main.m_clrVerifiedColor;
+                        parentLine.playerVRRig.playerText.color = Main.m_clrVerifiedColor;
+                    }
+                    else
+                    {
+                        parentLine.playerName.color = Color.white;
+                        parentLine.playerVRRig.playerText.color = Color.white;
+                    }
+                    myText.text = offText;
+                }
+                else
+                {
+                    parentLine.playerName.color = Main.m_clrFriendColor;
+                    parentLine.playerVRRig.playerText.color = Main.m_clrFriendColor;
+                    myText.text = onText;
+                }
+            }
+        }
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (touchTime > Time.time) return;
+            touchTime = Time.time + 0.25f;
+            GorillaTriggerColliderHandIndicator component = collider.GetComponent<GorillaTriggerColliderHandIndicator>();
+            if (component == null) return;
+
+            isOn = !isOn;
+            UpdateColor();
+            GorillaTagger.Instance.StartVibration(component.isLeftHand, GorillaTagger.Instance.tapHapticStrength / 2f, GorillaTagger.Instance.tapHapticDuration);
+
+            if (isOn)
+            {
+                Main.m_listCurrentSessionFriends.Add(parentLine.linePlayer.UserId);
+                PlayerPrefs.SetInt(parentLine.linePlayer.UserId + "_friend", 1);
+                parentLine.playerName.color = Main.m_clrFriendColor;
+                parentLine.playerVRRig.playerText.color = Main.m_clrFriendColor;
+                myText.text = onText;
+                return;
+            }
+
+            Main.m_listCurrentSessionFriends.Remove(parentLine.linePlayer.UserId);
+            PlayerPrefs.SetInt(parentLine.linePlayer.UserId + "_friend", 0);
+            myText.text = offText;
+            if (Main.IsVerified(parentLine.linePlayer.UserId))
+            {
+                parentLine.playerName.color = Main.m_clrVerifiedColor;
+                parentLine.playerVRRig.playerText.color = Main.m_clrVerifiedColor;
+            }
+            else
+            {
+                parentLine.playerName.color = Color.white;
+                parentLine.playerVRRig.playerText.color = Color.white;
+            }
+        }
+        public void UpdateColor()
+        {
+            if (isOn)
+            {
+                if (meshRenderer != null) meshRenderer.material = onMaterial;
+                myText.text = onText;
+            }
+            else
+            {
+                if (meshRenderer != null) meshRenderer.material = offMaterial;
+                myText.text = offText;
+            }
+        }
+    }
+}
--- GorillaFriends/Main.cs
+++ GorillaFriends/Main.cs
@@ -11,7 +11,7 @@ namespace GorillaFriends
     {
         internal static Main m_hInstance = null;
         internal static GameObject m_pScoreboardFriendBtn = null;
-        internal static GorillaPlayerLineButton m_pScoreboardFriendController = null;
+        internal static FriendButton m_pFriendButtonController = null;
         internal static List<string> m_listVerifiedUserIds = new List<string>();
         internal static List<string> m_listCurrentSessionFriends = new List<string>();
         internal static void Log(string msg) => m_hInstance.Logger.LogMessage(msg);
@@ -45,10 +45,6 @@ namespace GorillaFriends
         }
     }
 
-    public class ScoreboardLineFriendBtn : MonoBehaviour
-    {
-        public GorillaPlayerLineButton friendButton = null;
-    }
 
     [HarmonyPatch(typeof(GorillaScoreBoard))]
     [HarmonyPatch("Awake", MethodType.Normal)]
@@ -68,126 +64,25 @@ namespace GorillaFriends
                         Main.m_pScoreboardFriendBtn.transform.parent = __instance.scoreBoardLinePrefab.transform;
                         Main.m_pScoreboardFriendBtn.transform.name = "FriendButton";
                         Main.m_pScoreboardFriendBtn.transform.localPosition = new Vector3(17.5f, 0.0f, 0.0f);
-                        Main.m_pScoreboardFriendController = Main.m_pScoreboardFriendBtn.GetComponent<GorillaPlayerLineButton>();
-                        if (Main.m_pScoreboardFriendController != null)
+                        var controller = Main.m_pScoreboardFriendBtn.GetComponent<GorillaPlayerLineButton>();
+                        if (controller != null)
                         {
-                            __instance.scoreBoardLinePrefab.AddComponent<ScoreboardLineFriendBtn>().friendButton = Main.m_pScoreboardFriendController;
-                            Main.m_pScoreboardFriendController.offText = "ADD FRIEND";
-                            Main.m_pScoreboardFriendController.onText = "FRIEND!";
-                            Main.m_pScoreboardFriendController.myText.text = Main.m_pScoreboardFriendController.offText;
+                            Main.m_pFriendButtonController = Main.m_pScoreboardFriendBtn.AddComponent<FriendButton>();
+                            Main.m_pFriendButtonController.parentLine = controller.parentLine;
+                            Main.m_pFriendButtonController.offText = "ADD\nFRIEND";
+                            Main.m_pFriendButtonController.onText = "FRIEND!";
+                            Main.m_pFriendButtonController.myText = controller.myText;
+                            Main.m_pFriendButtonController.myText.text = Main.m_pFriendButtonController.offText;
+                            Main.m_pFriendButtonController.offMaterial = controller.offMaterial;
+                            Main.m_pFriendButtonController.onMaterial = new Material(controller.offMaterial);
+                            Main.m_pFriendButtonController.onMaterial.color = Main.m_clrFriendColor;
+
+                            GameObject.Destroy(controller);
                         }
                     }
                     return;
                 }
             }
-        }
-    }
-
-    [HarmonyPatch(typeof(GorillaPlayerScoreboardLine))]
-    [HarmonyPatch("Update", MethodType.Normal)]
-    internal class GorillaPlayerScoreboardLineUpdate
-    {
-        private static void Prefix(GorillaPlayerScoreboardLine __instance)
-        {
-            if (__instance.playerVRRig != null && __instance.linePlayer != null)
-            {
-                var tmp = __instance.GetComponent<ScoreboardLineFriendBtn>();
-                if (tmp == null || tmp.friendButton == null) return;
-                if (__instance.initialized == false)
-                {
-                    if (Main.IsVerified(__instance.linePlayer.UserId))
-                    {
-                        __instance.playerName.color = Main.m_clrVerifiedColor;
-                        __instance.playerVRRig.playerText.color = Main.m_clrVerifiedColor;
-                    }
-                    if (__instance.linePlayer.IsLocal) tmp.friendButton.gameObject.SetActive(false);
-                    else
-                    {
-                        if (Main.IsFriend(__instance.linePlayer.UserId))
-                        {
-                            Main.m_listCurrentSessionFriends.Add(__instance.linePlayer.UserId);
-                            __instance.playerName.color = Main.m_clrFriendColor;
-                            __instance.playerVRRig.playerText.color = Main.m_clrFriendColor;
-                            tmp.friendButton.isOn = true;
-                            tmp.friendButton.myText.text = tmp.friendButton.onText;
-                            tmp.friendButton.UpdateColor();
-                        }
-                    }
-                }
-                else
-                {
-                    if (!__instance.linePlayer.IsLocal && tmp.friendButton.isOn != Main.IsInFriendList(__instance.linePlayer.UserId))
-                    {
-                        tmp.friendButton.isOn = !tmp.friendButton.isOn;
-                        tmp.friendButton.UpdateColor();
-                        if (!tmp.friendButton.isOn)
-                        {
-                            if (Main.IsVerified(__instance.linePlayer.UserId))
-                            {
-                                __instance.playerName.color = Main.m_clrVerifiedColor;
-                                __instance.playerVRRig.playerText.color = Main.m_clrVerifiedColor;
-                            }
-                            else
-                            {
-                                __instance.playerName.color = Color.white;
-                                __instance.playerVRRig.playerText.color = Color.white;
-                            }
-                            tmp.friendButton.myText.text = tmp.friendButton.offText;
-                        }
-                        else
-                        {
-                            __instance.playerName.color = Main.m_clrFriendColor;
-                            __instance.playerVRRig.playerText.color = Main.m_clrFriendColor;
-                            tmp.friendButton.myText.text = tmp.friendButton.onText;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(GorillaPlayerLineButton))]
-    [HarmonyPatch("OnTriggerEnter", MethodType.Normal)]
-    internal class GorillaPlayerLineButtonTriggerEnter
-    {
-        private static bool Prefix(GorillaPlayerLineButton __instance, Collider collider)
-        {
-            if (__instance.transform.name == Main.m_pScoreboardFriendBtn.transform.name)
-            {
-                //if (!__instance.enabled || __instance.touchTime + __instance.debounceTime >= Time.time) return false;
-
-                GorillaTriggerColliderHandIndicator component = collider.GetComponent<GorillaTriggerColliderHandIndicator>();
-                if(component != null) GorillaTagger.Instance.StartVibration(component.isLeftHand, GorillaTagger.Instance.tapHapticStrength / 2f, GorillaTagger.Instance.tapHapticDuration);
-
-                __instance.isOn = !__instance.isOn;
-                __instance.UpdateColor();
-                if (__instance.isOn)
-                {
-                    Main.m_listCurrentSessionFriends.Add(__instance.parentLine.linePlayer.UserId);
-                    PlayerPrefs.SetInt(__instance.parentLine.linePlayer.UserId + "_friend", 1);
-                    __instance.parentLine.playerName.color = Main.m_clrFriendColor;
-                    __instance.parentLine.playerVRRig.playerText.color = Main.m_clrFriendColor;
-                    __instance.myText.text = __instance.onText;
-                }
-                else
-                {
-                    Main.m_listCurrentSessionFriends.Remove(__instance.parentLine.linePlayer.UserId);
-                    PlayerPrefs.SetInt(__instance.parentLine.linePlayer.UserId + "_friend", 0);
-                    __instance.myText.text = __instance.offText;
-                    if (Main.IsVerified(__instance.parentLine.linePlayer.UserId))
-                    {
-                        __instance.parentLine.playerName.color = Main.m_clrVerifiedColor;
-                        __instance.parentLine.playerVRRig.playerText.color = Main.m_clrVerifiedColor;
-                    }
-                    else
-                    {
-                        __instance.parentLine.playerName.color = Color.white;
-                        __instance.parentLine.playerVRRig.playerText.color = Color.white;
-                    }
-                }
-                return false;
-            }
-            return true;
         }
     }
 
@@ -198,17 +93,6 @@ namespace GorillaFriends
         private static void Prefix()
         {
             Main.m_listCurrentSessionFriends.Clear();
-        }
-    }
-
-    [HarmonyPatch(typeof(GorillaPlayerLineButton))]
-    [HarmonyPatch("Update", MethodType.Normal)]
-    internal class GorillaPlayerLineButtonUpdate
-    {
-        public static bool Prefix(GorillaPlayerLineButton __instance)
-        {
-            if (__instance == Main.m_pScoreboardFriendBtn) return false;
-            return true;
         }
     }
 }
